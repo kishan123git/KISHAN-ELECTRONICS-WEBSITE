b466bcab5ffd823466d4cb2dfc0417f0263303e0
kishan123git <patelkishan3101@gmail.com>
2025-03-02 16:23:49 +0530

    index.html, Aboutus.html, aboutstyle.css, style.css and images uploaded

---
+++ login.js
@@ -0,0 +1,30 @@
+
+
+
+
+// var objuser = [
+//     {
+//         username: "kishan",
+//         password: "patel"
+//     },{
+//         username: "kishanelectronics",
+//         password: "patidar"
+//     }
+// ]
+
+// function getinfo(){
+//     var username = document.getElementById("username").value;
+//     var password = document.getElementById("password").value;
+//     for(i=0; i<objuser.length; i++){
+//         if(username == objuser[i].username && password == objuser[i].password){
+//             alert(username+" "+"is login successfully");
+//             return
+//         }
+        
+//     }
+//     alert("Record not found.");
+// }
+
+
+
+var user = document.getElementById("")
